74cfaed147d3fc89525c1837c51743425184f2db
royschoonderbeek <royschoonderbeek9@gmail.com>
2026-01-14 11:50:20 +0100

    Added .env and bot

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Hello Twitch");
+require('dotenv').config();
+
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	channels: [ 'Roykiboyki' ],
+	identity: {
+		username: process.env.TWITCH_BOT_USERNAME,
+		password: process.env.TWITCH_OAUTH_TOKEN
+	}
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	const isNotBot = tags.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase;
+
+	if (isNotBot) {
+		client.say(channel, `Message "${message}" was sent by ${tags.username}`)
+	}
+	
+	console.log(`${tags['display-name']}: ${message}`);
+});
